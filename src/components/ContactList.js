744ce04be1ada7b2851bc49febb6c5f77c60e523
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-01 21:15:34 +0100

    user image added

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,17 @@
 import React from "react";
+import ContactCard from "./ContactCard"
 
 const ContactList = (props) => {
     console.log(props);
-    return(
-        <div className="ui celled list">
-            Contact List
-        </div>
-    );
+    const renderContactList = props.contacts.map((contact) => {
+        return(
+
+            <ContactCard contact={contact}></ContactCard>
+        );
+
+
+    });
+        return <div className="ui celled list">{renderContactList}</div>
+        
 };
 export default ContactList;
