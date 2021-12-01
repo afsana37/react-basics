70fc13f10f1a75ac6080c9ec9d621a4d538486dd
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-01 19:52:44 +0100

    Contactlist created

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+
+    const contacts =[
+        {
+          id: "1",
+          "name": "Chandler",
+          "email": "chanendlerbong@gmail.com"
+
+        },
+
+        {
+          id: "2",
+          "name": "Phoebe",
+          "email": "reginaphalangee@gmail.com"
+
+        },
+    ];
+    return (
+      <div className="ui container">
+        <Header />
+        <AddContact />
+        <ContactList contacts={contacts}/>
+        
+      </div>
+    );
+}
+
+export default App;
